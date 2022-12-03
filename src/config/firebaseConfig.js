70225bf947512205ac5185499261c48770066304
yash ambekar <yashamb444@gmail.com>
2022-12-03 22:08:05 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZrn2FQF8QKd3LHqp6qS7N1P9CUeaDzgM",
+    authDomain: "crypto-tracker-4119.firebaseapp.com",
+    projectId: "crypto-tracker-4119",
+    storageBucket: "crypto-tracker-4119.appspot.com",
+    messagingSenderId: "941160964010",
+    appId: "1:941160964010:web:3aa5bb41f8c52de7a415d3"
+  };
+
+  export default firebaseConfig;
